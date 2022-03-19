82926bd1cecdf34035dddcb16021f8e5d08bd2fc
tasim313 <mostasimmahmudtasim@gmail.com>
2022-03-19 19:44:50 +0600

    admin panel customise

--- Shop/admin.py
+++ Shop/admin.py
@@ -4,7 +4,15 @@ from .models import *
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'price', 'discount_price', 'category' ,)
+
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category='default')
+
+    change_category_to_default.short_description = 'Default Category'
+    list_display = ('id', 'title', 'price', 'discount_price', 'category',)
+    search_fields = ('title', 'price', 'discount_price', 'category',)
+    actions = ('change_category_to_default')
+    list_editable = ('title', 'price', 'discount_price', 'category')
 
 
 admin.site.register(Products, ProductAdmin)
@@ -12,6 +20,12 @@ admin.site.register(Products, ProductAdmin)
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'items', 'name')
+    search_fields = ('items', 'name')
 
 
 admin.site.register(Order, OrderAdmin)
+
+admin.site.site_header = "Ecommerce Site"
+admin.site.site_title = 'Online Shop'
+admin.site.site_index = 'ABC Shopping'
+
